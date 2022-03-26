caf7afab849776806bf06a396ef0bd296d683e7a
kekuopex <keksuxarevawex@gmail.com>
2022-03-26 07:56:38 +0300

    add solution exercises2

---
+++ exercises2.php
@@ -0,0 +1,7 @@
+<?php
+/*Write a PHP script to display the following strings. Go to the editor
+Sample String :
+'Tomorrow I \'ll learn PHP global variables.'
+'This is a bad command : del c:\\*.*' */
+echo "Tomorrow I \\'ll learn PHP global variables." . "\n";
+echo "This is a bad command : del c:\\*.*";
