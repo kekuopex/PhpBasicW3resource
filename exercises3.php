1775293e5fe8032dc353b3ff44f57052fc195b2e
kekuopex <keksuxarevawex@gmail.com>
2022-03-28 10:24:28 +0300

    add solution exercises3

---
+++ exercises3.php
@@ -0,0 +1,18 @@
+<?php
+/*Put the said variable into the title section, h3 tag and as an anchor text within an HTML document.*/
+$var = 'PHP Tutorial';
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>
+      <?php echo $var; ?>
+    </title>
+  </head>
+  <body>
+    <h3>
+      <?php echo $var; ?>
+    </h3>
+  </body>
+</html>
