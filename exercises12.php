ad733e537d49e5f1d105e0927b71dd43e82bcd4d
kekuopex <keksuxarevawex@gmail.com>
2022-04-05 15:06:43 +0300

    add solution exercises12

---
+++ exercises12.php
@@ -0,0 +1,13 @@
+<?php
+$email = "kekuopex@ggmail.com";
+
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  echo "$email is a valid email address";
+}
+else {
+  echo "$email is not a valid email address";
+}
+
+
+
+?>
